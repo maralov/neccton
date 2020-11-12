06092dc3e7891d03840a9cc0e05c1e28b5314615
maralov <aralov.mykhailo@gmail.com>
2020-11-12 23:44:31 +0200

    feedbacks $ news content

---
+++ mail.php
@@ -0,0 +1,12 @@
+<?php
+
+$recepient = "m.auer@neccton.com";
+$sitename = "Necctonwebsite";
+
+$name = trim($_POST["name"]);
+$email = trim($_POST["email"]);
+$text = trim($_POST["text"]);
+$message = "Name: $name \nE-mail: $email \nText: $text";
+
+$pagetitle = "New message from \"$sitename\"";
+mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
